82f4fab21c21d1dec107e3d977a32cb64f55ce0a
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-02-04 17:19:06 +0300

    added a new file images modified the About.js file modified the Home.js file modified the NavBar.js file

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>
+      <ul>    I am lovable.
+    I am a good person.
+    I deserve to be.
+    I am responsible for my own happiness.
+    I profoundly and deeply accept myself.
+    I have realistic expectations of myself.
+    My opinion of me is more important than others' opinion of me.</ul>
+   <img alt="I made this" src="images/pexels-pixabay-45201.jpg"></img>
+  
+    </p>
+    </div>;
 }
 
 export default About;
